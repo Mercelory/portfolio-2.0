6a64585d22c8db760575374149255cceaeae4ee8
mercelory <mercelory@gmail.com>
2025-02-05 20:16:08 +0200

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 function Navbar() {
   return (
     <nav className="fixed bottom-0 left-0 text-black w-full">
-      <div className="max-w-1/2 max-auto px-4 flex justify-center items-center h-16">
-        <a className="border-2">
+      <div className="w-full mx-auto px-15 flex justify-center items-center h-16">
+        <a className="border-2 p-1 m-3">
             Home
         </a>
-        <a className="border-2">
+        <a className="border-2 p-1 m-3">
             Work
         </a>
-        <a className="border-2">
+        <a className="border-2 p-1 m-3">
             Education
         </a>
-        <a className="border-2">
+        <a className="border-2 p-1 m-3">
             Skills
         </a>
-        <a className="border-2">
+        <a className="border-2 p-1 m-3">
             Projects
         </a>
         </div>
