18090541165d4852984a6a1dc3270290faafa6b9
Mercelory <mercelory@gmail.com>
2025-02-06 14:32:34 +0200

    mobile design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,16 @@ import { TbBrandTelegram, TbBrandGithub, TbBrandLinkedin, TbMail } from "react-i
 const Main = () => {
   return (
     <section class = ''>
-    <div className="flex-col justify-between w-9xl bottom-100 left-0 absolute mx-5">
-    <ul class = 'flex justify-between items-center w-80 h-10 ml-5 mb-5'>
-      <li className="text-5xl hover:text-zinc-300 hover:transition-transform duration-500"><a href = '#'><TbBrandTelegram /></a></li>
-      <li className="text-5xl text-3xl hover:text-zinc-300 hover:transition-transform duration-500"><a href = '#'> <TbBrandGithub /></a> </li>
-      <li className="text-5xl text-3xl hover:text-zinc-300 hover:transition-transform duration-500"><a href = '#'> <TbBrandLinkedin /></a> </li>
-      <li className="text-5xl text-3xl hover:text-zinc-300 hover:transition-transform duration-500"><a href = '#'><TbMail /></a> </li>
+    <div className="flex-col justify-between items-center top-50 left-0 relative md:top-70 lg:top-100">
+    <ul class = 'flex justify-between items-center w-40 ml-5 mb-2 md:w-60 lg:ml-10'>
+      <li className="hover:text-zinc-300 hover:transition-transform duration-500 text-3xl md:text-5xl"><a href = '#'><TbBrandTelegram /></a></li>
+      <li className="hover:text-zinc-300 hover:transition-transform duration-500 text-3xl md:text-5xl"><a href = '#'> <TbBrandGithub /></a> </li>
+      <li className="hover:text-zinc-300 hover:transition-transform duration-500 text-3xl md:text-5xl"><a href = '#'> <TbBrandLinkedin /></a> </li>
+      <li className="hover:text-zinc-300 hover:transition-transform duration-500 text-3xl md:text-5xl"><a href = '#'><TbMail /></a> </li>
     </ul>
-      <h1 class = 'text-9xl ml-5 font-sora'>Tischenko Dmitriy</h1>
-      <h2 class = 'text-4xl text-zinc-400 ml-10 mb-10'>Web-Developer</h2>
-      <p class = 'text-2xl ml-10 w-xl'>My name is Dmitry, I am an actively developing web applications. 
+      <h1 class = 'text-9xl ml-5 font-sora xs:text-4xl md:text-7xl lg:ml-10'>Tischenko Dmitriy</h1>
+      <h2 class = 'text-zinc-400 ml-5 mb-10 text-2xl md:text-4xl lg:ml-10'>Web-Developer</h2>
+      <p class = 'text-lg ml-5 w-xs md:text-2xl md:w-md lg:w-4xl lg:ml-10'>My name is Dmitry, I am an actively developing web applications. 
         I have 3 completed projects and many different developments in my portfolio. 
         I am going on developing more and more all the time.</p>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
 function Navbar() {
   return (
 <nav class = "fixed left-0 bottom-0 w-full h-10">
-    <ul class = 'flex justify-center items-center mx-auto '>
-      <a class = 'relative group mx-1 px-2 border '>
+    <ul class = 'flex justify-evenly items-center sm:justify-center'>
+      <a class = 'relative group px-2 border xs:text-sm sm:mr-3 sm:text-lg'>
         <span class = 'z-10 relative group-hover:text-white transition-transform duration-500'>Home</span>
         <span class = 'z-9 absolute inset-0 bg-black scale-y-0 group-hover:scale-y-100 transition-transform duration-500 origin-bottom'></span></a>
-        <a class = 'relative group mx-1 px-2 border '>
-        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500'>Work</span>
+        <a class = 'relative group px-2 border xs:text-sm sm:mr-3 sm:text-lg'>
+        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500 '>Work</span>
         <span class = 'z-9 absolute inset-0 bg-black scale-y-0 group-hover:scale-y-100 transition-transform duration-500 origin-bottom'></span></a>
-        <a class = 'relative group mx-1 px-2 border '>
-        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500'>Education</span>
+        <a class = 'relative group px-2 border xs:text-sm sm:mr-3 sm:text-lg'>
+        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500 '>Education</span>
         <span class = 'z-9 absolute inset-0 bg-black scale-y-0 group-hover:scale-y-100 transition-transform duration-500 origin-bottom'></span></a>
-        <a class = 'relative group mx-1 px-2 border '>
-        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500'>Skills</span>
+        <a class = 'relative group px-2 border xs:text-sm sm:mr-3 sm:text-lg'>
+        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500 '>Skills</span>
         <span class = 'z-9 absolute inset-0 bg-black scale-y-0 group-hover:scale-y-100 transition-transform duration-500 origin-bottom'></span></a>
-        <a class = 'relative group mx-1 px-2 border '>
-        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500'>Projects</span>
+        <a class = 'relative group px-2 border xs:text-sm sm:mr-3 sm:text-lg'>
+        <span class = 'z-10 relative group-hover:text-white transition-transform duration-500  '>Projects</span>
         <span class = 'z-9 absolute inset-0 bg-black scale-y-0 group-hover:scale-y-100 transition-transform duration-500 origin-bottom'></span></a>
     </ul>
 </nav>
